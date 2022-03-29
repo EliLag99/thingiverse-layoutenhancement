d2f2f9d29eb8d992e0700d7854c41950c9247616
EliLag99 <elias.lageder@outlook.com>
2022-03-29 22:22:16 +0100

    removed garbage code

--- scripts/scrappedcode.js
+++
@@ -1,7 +0,0 @@
-let actualCode =
-  "function initiatePopup() {" + '    console.log("Button clicked");' + "}";
-
-let script = document.createElement("script");
-script.textContent = actualCode;
-(document.head || document.documentElement).appendChild(script);
-script.remove();
